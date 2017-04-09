836b4a84d1250ac9816e5dc29c3cfc66320f93cd
L00094131 <L00094131@student.lyit.ie>
2017-04-09 21:21:06 +0100

    Third commit.

--- src/vehiclepkg/NoDoorsTest.java
+++ src/vehiclepkg/NoDoorsTest.java
@@ -25,5 +25,12 @@ public class NoDoorsTest {
 		int j = Car.getNoDoors();
 		assertEquals(2, j);
 	}
+	
+	@Test
+	public void setNoDoorstest3(){
+		Car.setNoDoors(2);
+		int j = Car.getNoDoors();
+		assertEquals(2, j);
+	}
 
 }
